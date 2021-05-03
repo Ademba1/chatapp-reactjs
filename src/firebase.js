1f107bf8ea4ff2f3142af0f82dbff896fe0c956c
Groot <oscarademba@gmail.com>
2021-05-03 15:17:58 +0300

    Create a React App using npx create-react-app chatapp

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCJCmDi1SjB8JckdR4f7KLd3zHq2E5v0TU",
+    authDomain: "chatapp-aca7e.firebaseapp.com",
+    projectId: "chatapp-aca7e",
+    storageBucket: "chatapp-aca7e.appspot.com",
+    messagingSenderId: "251189043612",
+    appId: "1:251189043612:web:edf2945aee9927e3092859",
+    measurementId: "G-74LT342GY4"
+  };
---
+++ tools.js
@@ -0,0 +1,10 @@
+//Whatsapp build tools
+
+//React
+//Firebase Firestore Realtime DB
+//Material UI
+//React Router
+//React Context API
+//Redux
+//Google Authentication
+//Deploy using Firebase
